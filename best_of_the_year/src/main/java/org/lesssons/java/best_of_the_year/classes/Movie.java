677b94de8f76f5c2fb6a1953d2b6f3779fc2c6fd
JacopoTrincanato <trincanatoj@gmail.com>
2025-03-12 12:00:34 +0100

    Create Movie.java and Song.java classes in classes folder

---
+++ best_of_the_year/src/main/java/org/lesssons/java/best_of_the_year/classes/Movie.java
@@ -0,0 +1,30 @@
+package org.lesssons.java.best_of_the_year.classes;
+
+public class Movie {
+    // attributi
+    private int id;
+    private String titolo;
+
+    // costruttore
+    public Movie(int id, String titolo) {
+        this.id = id;
+        this.titolo = titolo;
+    }
+
+    // metodi
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitolo() {
+        return this.titolo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+}
