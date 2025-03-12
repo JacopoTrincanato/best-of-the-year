e2855c3691e3d2087bb5c60afa584bd97ffc050c
JacopoTrincanato <trincanatoj@gmail.com>
2025-03-12 12:16:26 +0100

    create gestBestMovies and getBestSongs methods in AppControllers.java

--- best_of_the_year/src/main/java/org/lesssons/java/best_of_the_year/controllers/AppControllers.java
+++ best_of_the_year/src/main/java/org/lesssons/java/best_of_the_year/controllers/AppControllers.java
@@ -1,5 +1,9 @@
 package org.lesssons.java.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+
+import org.lesssons.java.best_of_the_year.classes.Movie;
+import org.lesssons.java.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +21,42 @@ public class AppControllers {
         // aggiungo il parametro nome
         model.addAttribute("name", name);
         return "homepage";
+    }
+
+    // creo i metodi privati getBestMovies and getBestSongs
+    private ArrayList<Movie> getBestMovies() {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        // creo alcuni film
+        Movie jurassicWorld = new Movie(1, "Jurassic World");
+        Movie jurassicPark = new Movie(2, "Jurassic Park");
+        Movie jurassicWorldDue = new Movie(3, "Jurassic World Due");
+        Movie jurassicParkDue = new Movie(4, "Jurassic Park Due");
+
+        // aggiungo i film a movies
+        movies.add(jurassicWorld);
+        movies.add(jurassicPark);
+        movies.add(jurassicWorldDue);
+        movies.add(jurassicParkDue);
+
+        return movies;
+    }
+
+    private ArrayList<Song> getBestSongs() {
+        ArrayList<Song> songs = new ArrayList<>();
+
+        // creo alcune canzoni
+        Song blackNirvana = new Song(1, "Black Nirvana");
+        Song comeUnTuono = new Song(2, "Come un Tuono");
+        Song mezzoRotto = new Song(3, "Mezzo Rotto");
+        Song ratata = new Song(4, "Ra Ta Ta");
+
+        // aggiungo le canzoni a songs
+        songs.add(blackNirvana);
+        songs.add(comeUnTuono);
+        songs.add(mezzoRotto);
+        songs.add(ratata);
+
+        return songs;
     }
 }
